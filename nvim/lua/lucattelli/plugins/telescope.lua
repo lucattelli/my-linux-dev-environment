720252da032bef6de5c4aae35b0c4539227274e4
Bruno Lucattelli <lucattelli@gmail.com>
2024-08-21 23:26:43 -0300

    Additional shortcuts

--- nvim/lua/lucattelli/plugins/telescope.lua
+++ nvim/lua/lucattelli/plugins/telescope.lua
@@ -1,19 +1,19 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()
-
+    config = function()
       local telescope = require('telescope')
 
       telescope.setup {
         extensions = {
           fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                             -- the default case_mode is "smart_case"
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
           }
         }
       }
@@ -22,10 +22,12 @@ return {
       telescope.load_extension('fzf')
 
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find_files"})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live_grep"})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope buffers"})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help_tags"})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>gst', builtin.git_status, {})
     end
   },
   {
