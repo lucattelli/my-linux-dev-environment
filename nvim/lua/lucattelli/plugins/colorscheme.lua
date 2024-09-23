10960a0377eaf91b23a46e7029549eb670078e0f
Bruno Lucattelli <bruno.lucattelli@newfront.com>
2024-09-23 11:01:18 -0300

    Install all colorschemes

--- nvim/lua/lucattelli/plugins/colorscheme.lua
+++ nvim/lua/lucattelli/plugins/colorscheme.lua
@@ -1,233 +1,233 @@
 return {
-  -- {
-  --   "nanotech/jellybeans.vim",
-  --   config = function()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme jellybeans")
-  --   end
-  -- },
-  -- {
-  --   "vim-scripts/less.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme less")
-  --   end
-  -- },
-  -- {
-  --   "vim-scripts/The-Vim-Gardener",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme gardener")
-  --   end
-  -- },
-  -- {
-  --   "vim-scripts/tango2",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme tango2")
-  --   end
-  -- },
-  -- {
-  --   "goatslacker/mango.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme mango")
-  --   end
-  -- },
-  -- {
-  --   "baskerville/bubblegum",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme bubblegum-256-dark")
-  --   end
-  -- },
-  -- {
-  --   "stuartherbert/vim-phix-colors",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme phix")
-  --   end
-  -- },
-  -- {
-  --   "csexton/spacemanspiff.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme spacemanspiff")
-  --   end
-  -- },
-  -- {
-  --   "gwutz/vim-materialtheme",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme materialtheme")
-  --   end
-  -- },
-  -- {
-  --   "kreeger/benlight",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme benlight")
-  --   end
-  -- },
-  -- {
-  --   "klappradla/klappradla.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme klappradla")
-  --   end
-  -- },
-  -- {
-  --   "juanedi/predawn.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme predawn")
-  --   end
-  -- },
-  -- {
-  --   "tstelzer/welpe.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme welpe")
-  --   end
-  -- },
-  -- {
-  --   "lewis6991/moonlight.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme moonlight")
-  --   end
-  -- },
-  -- {
-  --   "zacanger/angr.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme angr")
-  --   end
-  -- },
-  -- {
-  --   "erichain/vim-monokai-pro",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme monokai_pro")
-  --   end
-  -- },
-  -- {
-  --   "tomasiser/vim-code-dark",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme codedark")
-  --   end
-  -- },
-  -- {
-  --   "lunacookies/vim-substrata",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme substrata")
-  --   end
-  -- },
-  -- {
-  --   "antonk52/lake.nvim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme lake")
-  --   end
-  -- },
-  -- {
-  --   "elianiva/icy.nvim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme icy")
-  --   end
-  -- },
-  -- {
-  --   "shaunsingh/nord.nvim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme nord")
-  --   end
-  -- },
-  -- {
-  --   "n1ghtmare/noirblaze-vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme noirblaze")
-  --   end
-  -- },
-  -- {
-  --   "axvr/raider.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme raider")
-  --   end
-  -- },
-  -- {
-  --   "novasenco/vulpo",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme vulpo")
-  --   end
-  -- },
-  -- {
-  --   "thenitecoder/mountaineer.vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme mountaineer-grey")
-  --   end
-  -- },
-  -- {
-  --   "guilra/vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme omni")
-  --   end
-  -- },
+  {
+    "nanotech/jellybeans.vim",
+    -- config = function()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme jellybeans")
+    -- end
+  },
+  {
+    "vim-scripts/less.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme less")
+    -- end
+  },
+  {
+    "vim-scripts/The-Vim-Gardener",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme gardener")
+    -- end
+  },
+  {
+    "vim-scripts/tango2",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme tango2")
+    -- end
+  },
+  {
+    "goatslacker/mango.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme mango")
+    -- end
+  },
+  {
+    "baskerville/bubblegum",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme bubblegum-256-dark")
+    -- end
+  },
+  {
+    "stuartherbert/vim-phix-colors",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme phix")
+    -- end
+  },
+  {
+    "csexton/spacemanspiff.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme spacemanspiff")
+    -- end
+  },
+  {
+    "gwutz/vim-materialtheme",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme materialtheme")
+    -- end
+  },
+  {
+    "kreeger/benlight",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme benlight")
+    -- end
+  },
+  {
+    "klappradla/klappradla.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme klappradla")
+    -- end
+  },
+  {
+    "juanedi/predawn.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme predawn")
+    -- end
+  },
+  {
+    "tstelzer/welpe.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme welpe")
+    -- end
+  },
+  {
+    "lewis6991/moonlight.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme moonlight")
+    -- end
+  },
+  {
+    "zacanger/angr.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme angr")
+    -- end
+  },
+  {
+    "erichain/vim-monokai-pro",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme monokai_pro")
+    -- end
+  },
+  {
+    "tomasiser/vim-code-dark",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme codedark")
+    -- end
+  },
+  {
+    "lunacookies/vim-substrata",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme substrata")
+    -- end
+  },
+  {
+    "antonk52/lake.nvim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme lake")
+    -- end
+  },
+  {
+    "elianiva/icy.nvim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme icy")
+    -- end
+  },
+  {
+    "shaunsingh/nord.nvim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme nord")
+    -- end
+  },
+  {
+    "n1ghtmare/noirblaze-vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme noirblaze")
+    -- end
+  },
+  {
+    "axvr/raider.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme raider")
+    -- end
+  },
+  {
+    "novasenco/vulpo",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme vulpo")
+    -- end
+  },
+  {
+    "thenitecoder/mountaineer.vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme mountaineer-grey")
+    -- end
+  },
+  {
+    "guilra/vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme omni")
+    -- end
+  },
   {
     "haze/ambrosia.vim",
-    config = function ()
+    config = function()
       vim.opt.background = "dark"
       vim.cmd("colorscheme ambrosia")
     end
   },
-  -- {
-  --   "nikolvs/vim-sunbather",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme sunbather")
-  --   end
-  -- },
-  -- {
-  --   "jssee/vim-cortado",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme cortado")
-  --   end
-  -- },
-  -- {
-  --   "frenzyexists/aquarium-vim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme aquarium")
-  --   end
-  -- },
-  -- {
-  --   "xadillax/vim-mir2-colorscheme",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme mir2")
-  --   end
-  -- },
-  -- {
-  --   "domeee/mosel.nvim",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme mosel")
-  --   end
-  -- },
-  -- {
-  --   "antonio-hickey/citrus-mist",
-  --   config = function ()
-  --     vim.opt.background = "dark"
-  --     vim.cmd("colorscheme citrus-mist")
-  --   end
-  -- },
+  {
+    "nikolvs/vim-sunbather",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme sunbather")
+    -- end
+  },
+  {
+    "jssee/vim-cortado",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme cortado")
+    -- end
+  },
+  {
+    "frenzyexists/aquarium-vim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme aquarium")
+    -- end
+  },
+  {
+    "xadillax/vim-mir2-colorscheme",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme mir2")
+    -- end
+  },
+  {
+    "domeee/mosel.nvim",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme mosel")
+    -- end
+  },
+  {
+    "antonio-hickey/citrus-mist",
+    -- config = function ()
+    --   vim.opt.background = "dark"
+    --   vim.cmd("colorscheme citrus-mist")
+    -- end
+  },
 }
