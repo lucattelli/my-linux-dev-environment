0dbffc6108321166608a3bc1ab6a29158cb9a2ae
Bruno Lucattelli <lucattelli@gmail.com>
2025-03-31 23:21:24 -0300

    Add pinner

---
+++ nvim/lua/lucattelli/plugins/pinner.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "lucattelli/pinner.nvim",
+    config = function()
+      require("pinner").setup()
+    end
+  }
+}
+-- return {
+--   { dir = "~/Programming/pinner.nvim",
+--     config = function()
+--       require("pinner").setup()
+--     end
+--   }
+-- }
